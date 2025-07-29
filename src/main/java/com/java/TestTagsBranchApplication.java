90faca79686cd485629ef76385d484f982af4bf3
mohammad rashidi <sufian.jde@gmail.com>
2025-07-29 00:56:13 -0400

    1C

---
+++ src/main/java/com/java/TestTagsBranchApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTagsBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTagsBranchApplication.class, args);
+	}
+
+}
